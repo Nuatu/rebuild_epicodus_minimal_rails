50e1be3d266f737b8ee29f0f9db3a078987a3df1
Randy Leighton & Christian Danielsen <student@epicodus.com>
2014-08-28 10:43:05 -0700

    validate presence of context

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,5 @@
 class Lesson < ActiveRecord::Base
   validates :name, :presence => true
   validates :number, :presence => true, numericality: {only_integer: true}
+  validates :context, :presence => true
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -4,4 +4,5 @@ describe Lesson do
   it { should validate_presence_of :name }
   it { should validate_presence_of :number }
   it { should validate_numericality_of(:number).only_integer }
+  it { should validate_presence_of :context}
 end
