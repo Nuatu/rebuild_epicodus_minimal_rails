bf23e30b8de329fbe65dd32fb69adf526b70c564
randy leighton & Nuatu Tseggai <student@epicodus.com>
2014-08-29 10:57:46 -0700

    add index to controller and index.html.erb

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,3 +1,29 @@
 class SectionsController < ApplicationController
 
+  def index
+    @sections = Section.all
+    @section = Section.new
+    render('index.html.erb')
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
